cbad92c441abe5cbceeb9cc4b34ebaad4b9b7423
Sarah Roberts <sarahrobe@comcast.net>
2024-07-12 13:55:37 -0500

    Initial Commit

---
+++ githubTutorial/githubTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubTutorialApp.swift
+//  githubTutorial
+//
+//  Created by Sarah Roberts on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct githubTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
